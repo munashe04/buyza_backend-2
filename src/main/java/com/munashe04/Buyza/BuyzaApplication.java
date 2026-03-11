4e7f488e4c2476d4e686102b78e688b4f23606e9
munashe04 <Thondo@qdb.qa>
2026-03-11 13:02:34 +0200

    fix: add EnableJpaRepositories and database config

--- src/main/java/com/munashe04/Buyza/BuyzaApplication.java
+++ src/main/java/com/munashe04/Buyza/BuyzaApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
+@EnableJpaRepositories(basePackages = "com.munashe04.Buyza.repository")
 @SpringBootApplication(exclude = {
 		DataSourceAutoConfiguration.class,
 		DataSourceTransactionManagerAutoConfiguration.class,
